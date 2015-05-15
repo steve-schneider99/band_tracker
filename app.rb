48e35de63014293835d61d2a7d68d9c69ff1d12e
Steve Schneider <steve.schneider99@gmail.com>
2015-05-15 10:32:13 -0700

    add update and delete functions to band_list for forms, app.rb for code and routing

--- app.rb
+++ app.rb
@@ -14,12 +14,28 @@ end
 
 get('/band/:id') do
   @band = Band.find(params.fetch("id").to_i)
+  @venues = Venue.all()
   erb(:band_detail)
 end
 
 post('/add_band') do
   name = params.fetch("band_name")
   Band.create({:name => name})
+  @bands = Band.all()
+  erb(:band_list)
+end
+
+patch('/band/:id/edit') do
+  name = params.fetch("name")
+  @band = Band.find(params.fetch("id").to_i)
+  @band.update({:name => name})
+  @venues = Venue.all()
+  erb(:band_detail)
+end
+
+delete('/band/:id/delete') do
+  @band_delete = Band.find(params.fetch("id").to_i)
+  @band_delete.delete()
   @bands = Band.all()
   erb(:band_list)
 end
